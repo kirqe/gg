46ed58a26c7e8263138425ecfdfda674aa8ebfa7
railsr <ikll7@yahoo.com>
2016-10-02 20:39:24 +0400

    pagination / 4 per page

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -31,4 +31,33 @@ $(function() {
 
 	$(window).resize(sameHeightTextBlocks);
 
-});
+/* Load more posts on button click */
+
+	$(".loadMoreOne, .loadMore").click(loadMorePosts);
+	$(".loadMoreOne, .loadMore").click(scrollBot);
+
+	function loadMorePosts() {
+		var _this = this;
+		var $blogContainer = $("#blogContainer");
+		var nextPage = parseInt($blogContainer.attr("data-page")) + 1;
+		var totalPages = parseInt($blogContainer.attr("data-totalPages"));
+
+		$.get("/blog/page" + nextPage, function (data) {
+			var htmlData = $.parseHTML(data);
+			var $posts = $(htmlData).find(".post");
+
+			$blogContainer.attr("data-page", nextPage).append($posts);
+
+			if ($blogContainer.attr("data-totalPages") == nextPage) {
+				$(".loadMoreOne, .loadMore").remove();
+			}
+			$(".loadMoreOneWrap").remove();
+			sameHeightTextBlocks();
+		});
+	}
+
+	function scrollBot(){
+		$('html, body').animate({ scrollTop: $(document).height() }, 1200)
+		return;
+	}
+})
